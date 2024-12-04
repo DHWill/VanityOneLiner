4081e7d3ab55a274f8165f1738560efef651b079
DHWill <will@dominicharris.studio>
2024-12-04 11:00:26 +0000

    add back in tungsten

--- runCameraFullScreen.sh
+++ runCameraFullScreen.sh
@@ -5,5 +5,5 @@ xset s noblank
 xset -dpms
 
 #To Start Camera
-raspivid -w 720 -h 720 --exposure auto --flicker 50hz --awb tungsten -f -t 0
+raspivid -w 720 -h 850 --exposure auto --flicker 50hz --awb tungsten -f -t 0
 #raspivid -w 720 -h 720 --exposure auto --flicker 50hz -f -t 0
